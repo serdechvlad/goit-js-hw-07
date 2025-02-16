4ff5de112a6719104c55406b52a44e3d3791415c
serdechvlad <serdechvlad94@gmail.com>
2025-02-16 18:11:34 +0100

    f
    f

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const categoriesList = document.querySelector('#categories');
+
+const categoriesItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach((category) => {
+  const title = category.querySelector('h2').textContent;
+  const elementsCount = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
---
+++ js/task-6.js
@@ -0,0 +1,46 @@
+'use strict';
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value, 10);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.margin = '5px';
+    div.style.display = 'inline-block';
+    size += 10;
+    fragment.appendChild(div);
+  }
+
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
